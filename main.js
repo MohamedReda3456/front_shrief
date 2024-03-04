b6bfa121a239493c476361d9eda2a1f3989b8789
Mohamed Reda <148896247+MohamedReda3456@users.noreply.github.com>
2024-03-04 22:27:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let a=document.getElementById("moon");
+let b=document.getElementById("sun");
+let c=document.getElementById("body");
+let v=document.getElementById("mood");
+a.onclick=function(){
+    c.style.backgroundColor = " #252323";
+    
+}
+b.onclick=function(){
+    c.style.backgroundColor ="white";
+    
+}
